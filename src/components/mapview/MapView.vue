3e9fd6c55872815f7fc54b2d1225bf0fe28c2843
Edson Rosales <edsonme9@gmail.com>
2023-09-14 13:59:10 -0600

    feat[MapView]: Loading indicator & ref to the map conatiner

--- src/components/mapview/MapView.vue
+++ src/components/mapview/MapView.vue
@@ -1,15 +1,41 @@
 <script lang="ts" src="./MapView.ts" />
 
 <template>
-  <h1>Map View</h1>
-  <p>
-    {{ isLoading }}
-    {{ userLocation }}
-    {{ isUserlocationReady }}
-  </p>
+  <div
+    v-if="!isUserlocationReady"
+    class="loading-map d-flex justify-content-center align-items-center"
+  >
+    <div class="text-center">
+      <h3>Cargando...</h3>
+      <span>Obteniendo su geolocalización</span>
+    </div>
+  </div>
+
+  <div
+    v-show="isUserlocationReady"
+    class="map-container"
+    ref="mapElement"
+  />
+
 </template>
 
 
 <style scoped>
+  .map-container {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    background-color: red;
+  }
 
+  .loading-map {
+    color: white;
+    position: fixed;
+    top: 0px;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 9999;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
 </style>
