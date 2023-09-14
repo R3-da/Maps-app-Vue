4b0753bc032a286394fb0832103e4fa0f4b3ba46
Edson Rosales <edsonme9@gmail.com>
2023-09-14 13:37:02 -0600

    feat[MapView]: Template component & logic component to show the Map into the view using the composable function 'usePlacesStore'

---
+++ src/components/mapview/MapView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" src="./MapView.ts" />
+
+<template>
+  <h1>Map View</h1>
+  <p>
+    {{ isLoading }}
+    {{ userLocation }}
+    {{ isUserlocationReady }}
+  </p>
+</template>
+
+
+<style scoped>
+
+</style>
