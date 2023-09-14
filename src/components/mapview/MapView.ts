4cc6517c7c50a82ca21ffc24f71932ae6c15ce66
Edson Rosales <edsonme9@gmail.com>
2023-09-14 14:57:49 -0600

    feat[MapView]: Show the component of Mapbox & fixed some issues with the rendering of the Map

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,6 @@
+import { defineComponent, onMounted, ref, watch } from 'vue';
 import { usePlacesStore } from '@/composables';
-import { defineComponent, onMounted, ref } from 'vue';
+import Mapboxgl from 'mapbox-gl';
 
 export default defineComponent({
   name: 'MapView',
@@ -8,9 +9,29 @@ export default defineComponent({
     const mapElement = ref<HTMLDivElement>();
     const { userLocation, isUserlocationReady } = usePlacesStore();
 
+    const initMap = async () => {
+      // Guard instances
+      if ( !mapElement.value ) throw new Error('Element not found!');
+      if ( !userLocation.value ) throw new Error('User location not found!');
+      
+      await Promise.resolve();  // <--- This line helps to finish all async process to make a right build of the Map rendered on the browser
+
+      const map = new Mapboxgl.Map({
+        container: mapElement.value, // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 15, // starting zoom
+      });
+    }
+
     onMounted(() => {
-      console.log(mapElement.value);
+      if (isUserlocationReady.value) 
+        return initMap();
     });
+
+    watch( isUserlocationReady, ( newVal ) => {
+      if (isUserlocationReady.value) initMap();
+    })
 
     return {
       isUserlocationReady,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWRkcm9zYWxlcyIsImEiOiJjbG1qbTJsNjUwNGlwMmtxaWJtNjQ2eG91In0._O6LyUEYcxC8JY_Eh5hlbQ';
 
 if (!navigator.geolocation) {
-    alert('Tu navegador necesita poder acceder a la Geolocalización');
-    throw new Error('Tu navegador necesita poder acceder a la Geolocalización');
+  alert('Tu navegador necesita poder acceder a la Geolocalización');
+  throw new Error('Tu navegador necesita poder acceder a la Geolocalización');
 }
 
 createApp(App)
