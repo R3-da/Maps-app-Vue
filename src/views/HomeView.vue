85ce88216626229ad3f6e421167c014c224a96df
Edson Rosales <edsonme9@gmail.com>
2023-09-19 16:40:05 -0600

    feat[ButtonToCenterLocation]: Implementation of a new button to move to the main central location(User's location) with a smooth animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <MapView />
+  <MyLocationBtn />
 </template>
 
 <script lang="ts">
@@ -7,10 +8,11 @@ import { defineComponent } from 'vue';
 import { usePlacesStore } from "@/composables/usePlacesStore";
 
 import MapView from "@/components/mapview/MapView.vue";
+import MyLocationBtn from "@/components/my-location-btn/MyLocationBtn.vue";
 
 export default defineComponent({
   name: 'HomeView',
-  components: { MapView },
+  components: { MapView, MyLocationBtn },
   setup() {
     usePlacesStore();
   }
