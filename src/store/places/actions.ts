cea3b87b0e4cb02d22eb05c21236fd2066986501
Edson Rosales <edsonme9@gmail.com>
2023-09-20 14:39:12 -0600

    feat[SavePlaces]: Functionality to keep save the places searched by the user in the Vuex store

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -2,6 +2,7 @@ import { ActionTree } from 'vuex';
 import { PlacesState } from './state';
 import { StateInterface } from '../index';
 import { searchApi } from '@/apis';
+import { PlacesResponse, Feature } from '@/interfaces/places';
 
 
 const actions: ActionTree<PlacesState, StateInterface> = {
@@ -16,16 +17,27 @@ const actions: ActionTree<PlacesState, StateInterface> = {
         );
     },
 
-    async searchPlacesByTerm({ commit, state }, query: string) {
+    async searchPlacesByTerm({ commit, state }, query: string): Promise<Feature[]> {
+        if (query.length === 0) {
+            commit('setPlaces', []);
+            return [];
+        }
 
-        console.log('Vuex: ', query);
+        if (!state.userLocation) {
+            // Handle this error in a better way...An error handler for example
+            throw new Error('La ubicación del usuario no se logró obtener');
+        }
 
-        // const resp = await searchApi(`/${ query }.json`, {
-        //     params: {
-        //         proximity: state.userLocation?.join(',')
-        //     }
-        // });
-        // console.log(resp.data);
+        // Loading
+        commit('setIsLoadingPlaces');
+
+        const resp = await searchApi.get<PlacesResponse>(`/${ query }.json`, {
+            params: {
+                proximity: state.userLocation?.join(',')
+            }
+        });
+        commit('setPlaces', resp.data.features);
+        return resp.data.features;
     }
 }
 
