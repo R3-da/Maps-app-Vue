cf7f20c38047603618afab5426219d439f102389
Edson Rosales <edsonme9@gmail.com>
2023-09-19 18:11:21 -0600

    feat[SearchBar]: Design of SearchBar component & SearchResults component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <MapView />
+  <SearchBar />
   <MyLocationBtn />
 </template>
 
@@ -8,11 +9,12 @@ import { defineComponent } from 'vue';
 import { usePlacesStore } from "@/composables/usePlacesStore";
 
 import MapView from "@/components/mapview/MapView.vue";
+import SearchBar from "@/components/searchbar/SearchBar.vue";
 import MyLocationBtn from "@/components/my-location-btn/MyLocationBtn.vue";
 
 export default defineComponent({
   name: 'HomeView',
-  components: { MapView, MyLocationBtn },
+  components: { MapView, MyLocationBtn, SearchBar },
   setup() {
     usePlacesStore();
   }
